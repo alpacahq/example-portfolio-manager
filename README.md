a14c1c3b371f62b873f4b45e50c4293d061cdc5a
Hitoshi Harada <hitoshi@alpaca.markets>
2019-10-11 01:11:05 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,41 @@
+# Portfolio Manager For Alpaca Trade API
+
+The script [`portfolio_manager.py`](./portfolio_manager.py) contains a class that
+makes it easy to manage busket of stocks based on the shares or weight for each symbol name.
+It is designed so that you can feed your own desired portfolio either by
+calculation or simply from CSV.
+
+## Usage
+
+You need to obtain Alpaca API key, found in the [dashboard](https://app.alpaca.markets/) and set
+it in the environment variables. Please read [API documents](https://docs.alpaca.markets/) or
+[Python SDK documents](https://github.com/alpacahq/alpaca-trade-api-python) for more details.
+
+The `PortfolioManager` class implements the following methods.
+
+- add_items(items)
+  Add desired portfolio structure, in two dimentional list.
+- rebalance(order_style, timeout=60)
+  Start rebalancing based on the shares by sending orders now. `order_style` can be either "timeout" or "block"
+- percent_rebalance(order_style, timeout=60)
+  Start rebalancing based on the percentage by sending orders now. `order_style` can be either "timeout" or "block"
+
+The `timeout` order style times out if orders are not executed in the specified timeout seconds.
+The `block` order style blocks the process indifinitely until orders are executed.
+
+## Example Code
+
+```py
+import os
+
+os.environ['APCA_API_KEY_ID'] = 'API_KEY_ID'
+os.environ['APCA_API_SECRET_KEY'] = 'API_SECRET_KEY'
+
+manager = PortfolioManager()
+# Hedging SPY with GLD 1:1
+manager.add_items([
+    ['SPY', 0.5],
+    ['GLD', 0.5],
+])
+manager.percent_rebalance('block')
+```
---
+++ portfolio_manager.py
@@ -0,0 +1,247 @@
+import alpaca_trade_api as tradeapi
+import os
+import threading
+import time
+
+
+class PortfolioManager():
+    def __init__(self):
+        self.api = tradeapi.REST()
+
+        self.r_positions = {}
+
+    def format_percent(self, num):
+        if(str(num)[-1] == "%"):
+            return float(num[:-1]) / 100
+        else:
+            return float(num)
+
+    def clear_orders(self):
+        try:
+            self.api.cancel_all_orders()
+            print("All open orders cancelled.")
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
+    def add_items(self, data):
+        ''' Expects a list of lists containing two items: symbol and position qty/pct
+
+        '''
+        for row in data:
+            self.r_positions[row[0]] = [row[1], 0]
+
+    def percent_rebalance(self, order_style, timeout=60):
+        print(f"Desired positions: ")
+        for sym in self.r_positions:
+            print(f"{sym} - {self.r_positions.get(sym)[0]} of portfolio.")
+        print()
+
+        positions = self.api.list_positions()
+        account = self.api.get_account()
+        portfolio_val = float(account.portfolio_value)
+        for sym in self.r_positions:
+            price = self.api.get_barset(sym, "minute", 1)[sym][0].c
+            self.r_positions[sym][0] = int(
+                self.format_percent(
+                    self.r_positions.get(sym)[0]) * portfolio_val / price)
+
+        print(f"Current positions: ")
+        for position in positions:
+            print(
+                f"{position.symbol} - {round(float(position.market_value) / portfolio_val * 100, 2)}% of portfolio.")
+        print()
+
+        self.clear_orders()
+
+        print("Clearing extraneous positions.")
+        for position in positions:
+            if(self.r_positions.get(position.symbol)):
+                self.r_positions.get(position.symbol)[1] = int(position.qty)
+            else:
+                self.send_basic_order(
+                    position.symbol, position.qty, ("buy", "sell")[
+                        position.side == "long"])
+        print()
+
+        if(order_style == "send"):
+            for sym in self.r_positions:
+                qty = self.r_positions.get(
+                    sym)[0] - self.r_positions.get(sym)[1]
+                self.send_basic_order(sym, qty, ("buy", "sell")[qty < 0])
+        elif(order_style == "timeout"):
+            threads = []
+            for i, sym in enumerate(self.r_positions):
+                qty = self.r_positions.get(
+                    sym)[0] - self.r_positions.get(sym)[1]
+                threads.append(
+                    threading.Thread(
+                        target=self.timeout_execution, args=(
+                            sym, qty, ("buy", "sell")[
+                                qty < 0], self.r_positions.get(sym)[0], timeout)))
+                threads[i].start()
+
+            for i in range(len(threads)):
+                threads[i].join()
+        elif(order_style == "block"):
+            threads = []
+            for i, sym in enumerate(self.r_positions):
+                qty = self.r_positions.get(
+                    sym)[0] - self.r_positions.get(sym)[1]
+                threads.append(
+                    threading.Thread(
+                        target=self.confirm_full_execution, args=(
+                            sym, qty, ("buy", "sell")[
+                                qty < 0], self.r_positions.get(sym)[0])))
+                threads[i].start()
+
+            for i in range(len(threads)):
+                threads[i].join()
+
+    def rebalance(self, order_style, timeout=60):
+        print(f"Desired positions: ")
+        for sym in self.r_positions:
+            print(f"{sym} - {self.r_positions.get(sym)[0]} shares.")
+        print("\n")
+
+        positions = self.api.list_positions()
+
+        print(f"Current positions: ")
+        for position in positions:
+            print(f"{position.symbol} - {position.qty} shares owned.")
+        print()
+
+        self.clear_orders()
+
+        print("Clearing extraneous positions.")
+        for position in positions:
+            if(self.r_positions.get(position.symbol)):
+                self.r_positions[position.symbol][1] = int(position.qty)
+            else:
+                self.send_basic_order(
+                    position.symbol, position.qty, ("buy", "sell")[
+                        position.side == "long"])
+        print()
+
+        if(order_style == "send"):
+            for sym in self.r_positions:
+                qty = int(self.r_positions.get(sym)[
+                          0]) - self.r_positions.get(sym)[1]
+                self.send_basic_order(sym, qty, ("buy", "sell")[qty < 0])
+        elif(order_style == "timeout"):
+            threads = []
+            for i, sym in enumerate(self.r_positions):
+                qty = int(self.r_positions.get(sym)[
+                          0]) - self.r_positions.get(sym)[1]
+                threads.append(
+                    threading.Thread(
+                        target=self.timeout_execution, args=(
+                            sym, qty, ("buy", "sell")[
+                                qty < 0], self.r_positions.get(sym)[0], timeout)))
+                threads[i].start()
+
+            for i in range(len(threads)):
+                threads[i].join()
+        elif(order_style == "block"):
+            threads = []
+            for i, sym in enumerate(self.r_positions):
+                qty = int(self.r_positions.get(sym)[
+                          0]) - self.r_positions.get(sym)[1]
+                threads.append(
+                    threading.Thread(
+                        target=self.confirm_full_execution, args=(
+                            sym, qty, ("buy", "sell")[
+                                qty < 0], self.r_positions.get(sym)[0])))
+                threads[i].start()
+
+            for i in range(len(threads)):
+                threads[i].join()
+
+    def send_basic_order(self, sym, qty, side):
+        qty = int(qty)
+        if(qty == 0):
+            return
+        q2 = 0
+        try:
+            position = self.api.get_position(sym)
+            curr_pos = int(position.qty)
+            if((curr_pos + qty > 0) != (curr_pos > 0)):
+                q2 = curr_pos
+                qty = curr_pos + qty
+        except BaseException:
+            pass
+        try:
+            if q2 != 0:
+                self.api.submit_order(sym, abs(q2), side, "market", "gtc")
+                try:
+                    self.api.submit_order(sym, abs(qty), side, "market", "gtc")
+                except Exception as e:
+                    print(
+                        f"Error: {str(e)}. Order of | {abs(qty) + abs(q2)} {sym} {side} | partially sent ({abs(q2)} shares sent).")
+                    return False
+            else:
+                self.api.submit_order(sym, abs(qty), side, "market", "gtc")
+            print(f"Order of | {abs(qty) + abs(q2)} {sym} {side} | submitted.")
+            return True
+        except Exception as e:
+            print(
+                f"Error: {str(e)}. Order of | {abs(qty) + abs(q2)} {sym} {side} | not sent.")
+            return False
+
+    def confirm_full_execution(self, sym, qty, side, expected_qty):
+        sent = self.send_basic_order(sym, qty, side)
+        if(not sent):
+            return
+
+        executed = False
+        while(not executed):
+            try:
+                position = self.api.get_position(sym)
+                if int(position.qty) == int(expected_qty):
+                    executed = True
+                else:
+                    print(f"Waiting on execution for {sym}...")
+                    time.sleep(20)
+            except BaseException:
+                print(f"Waiting on execution for {sym}...")
+                time.sleep(20)
+        print(
+            f"Order of | {abs(qty)} {sym} {side} | completed.  Position is now {expected_qty} {sym}.")
+
+    def timeout_execution(self, sym, qty, side, expected_qty, timeout):
+        sent = self.send_basic_order(sym, qty, side)
+        if(not sent):
+            return
+        output = []
+        executed = False
+        timer = threading.Thread(
+            target=self.set_timeout, args=(
+                timeout, output))
+        timer.start()
+        while(not executed):
+            if(len(output) == 0):
+                try:
+                    position = self.api.get_position(sym)
+                    if int(position.qty) == int(expected_qty):
+                        executed = True
+                    else:
+                        print(f"Waiting on execution for {sym}...")
+                        time.sleep(20)
+                except BaseException:
+                    print(f"Waiting on execution for {sym}...")
+                    time.sleep(20)
+            else:
+                timer.join()
+                try:
+                    position = self.api.get_position(sym)
+                    curr_qty = position.qty
+                except BaseException:
+                    curr_qty = 0
+                print(
+                    f"Process timeout at {timeout} seconds: order of | {abs(qty)} {sym} {side} | not completed. Position is currently {curr_qty} {sym}.")
+                return
+        print(
+            f"Order of | {abs(qty)} {sym} {side} | completed.  Position is now {expected_qty} {sym}.")
+
+    def set_timeout(self, timeout, output):
+        time.sleep(timeout)
+        output.append(True)
